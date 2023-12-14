62bf1dad986c0ddb58c42382a0ba3e68ce34d2b1
valera <valera7979@mail.ru>
2023-12-14 17:27:49 +0400

    ignore fault tests

--- src/test/groovy/com/example/demo/controller/PostControllerTest.groovy
+++ src/test/groovy/com/example/demo/controller/PostControllerTest.groovy
@@ -5,6 +5,7 @@ import com.example.demo.entity.Post
 import com.example.demo.entity.User
 import com.example.demo.service.PostService
 import com.example.demo.service.UserService
+import org.junit.jupiter.api.Disabled
 import org.junit.runner.RunWith
 import org.spockframework.spring.SpringBean
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.ResponseEntity
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Ignore
 import spock.lang.Specification
 import org.springframework.http.HttpStatus
 import com.example.demo.controller.dto.PostDto
@@ -39,113 +41,118 @@ class PostControllerTest extends Specification {
         postController
     }
 
-//    def "should get post by id"() {
-//        given:
-//        def postId = "postId"
-//        def jwt = "token"
-//        def currentUser = new User(id: 1, fullName: "testUser")
-//        def post = new Post(id: postId)
-//
-//        userService.findUserProfileByJwt(jwt) >> currentUser
-//        postService.findById(postId) >> post
-//
-//        when:
-//        def result = postController.getPost(postId, jwt)
-//
-//        then:
-//        1 * userService.findUserProfileByJwt(jwt)
-//        1 * postService.findById(postId)
-//
-//        result instanceof ResponseEntity
-//        result.statusCode == HttpStatus.OK
-//        result.body == PostDto.toDto(currentUser, post)
-//    }
-//
-//    def "should create a post"() {
-//        given:
-//        def jwt = "token"
-//        def currentUser = new User(id: 1, fullName: "testUser")
-//        def post = new Post()
-//        def createdPost = new Post()
-//
-//        userService.findUserProfileByJwt(jwt) >> currentUser
-//        postService.createPost(post, currentUser) >> createdPost
-//
-//        when:
-//        def result = postController.createPost(post, jwt)
-//
-//        then:
-//        1 * userService.findUserProfileByJwt(jwt)
-//        1 * postService.createPost(post, currentUser)
-//
-//        result instanceof ResponseEntity
-//        result.statusCode == HttpStatus.CREATED
-//        result.body == PostDto.toDto(currentUser, createdPost)
-//    }
-//
-//    def "should update a post"() {
-//        given:
-//        def postId = "postId"
-//        def jwt = "token"
-//        def currentUser = new User(id: 1, fullName: "testUser")
-//        def post = new Post()
-//        def editedPost = new Post()
-//
-//        userService.findUserProfileByJwt(jwt) >> currentUser
-//        postService.editPost(postId, post, currentUser) >> editedPost
-//
-//        when:
-//        def result = postController.updatePost(postId, post, jwt)
-//
-//        then:
-//        1 * userService.findUserProfileByJwt(jwt)
-//        1 * postService.editPost(postId, post, currentUser)
-//
-//        result instanceof ResponseEntity
-//        result.statusCode == HttpStatus.ACCEPTED
-//        result.body == PostDto.toDto(currentUser, editedPost)
-//    }
-//
-//    def "should create a comment"() {
-//        given:
-//        def jwt = "token"
-//        def currentUser = new User(id: 1, fullName: "testUser")
-//        def commentRequest = new CommentRequest()
-//        def post = new Post()
-//
-//        userService.findUserProfileByJwt(jwt) >> currentUser
-//        postService.createComment(commentRequest, currentUser) >> post
-//
-//        when:
-//        def result = postController.createComment(commentRequest, jwt)
-//
-//        then:
-//        1 * userService.findUserProfileByJwt(jwt)
-//        1 * postService.createComment(commentRequest, currentUser)
-//
-//        result instanceof ResponseEntity
-//        result.statusCode == HttpStatus.CREATED
-//        result.body == PostDto.toDto(currentUser, post)
-//    }
-//
-//    def "should delete a post"() {
-//        given:
-//        def postId = "postId"
-//        def jwt = "token"
-//        def currentUser = new User(id: 1, fullName: "testUser")
-//
-//        userService.findUserProfileByJwt(jwt) >> currentUser
-//        postService.deletePostById(postId, currentUser)
-//
-//        when:
-//        def result = postController.deletePost(postId, jwt)
-//
-//        then:
-//        1 * userService.findUserProfileByJwt(jwt)
-//        1 * postService.deletePostById(postId, currentUser)
-//
-//        result instanceof ResponseEntity
-//        result.statusCode == HttpStatus.NO_CONTENT
-//        result.body == null
-//    }
+    @Ignore("Temporarily disabled due to userService mock issue. Need to fix")
+    def "should get post by id"() {
+        given:
+        def postId = "postId"
+        def jwt = "token"
+        def currentUser = new User(id: 1, fullName: "testUser")
+        def post = new Post(id: postId)
+
+        userService.findUserProfileByJwt(jwt) >> currentUser
+        postService.findById(postId) >> post
+
+        when:
+        def result = postController.getPost(postId, jwt)
+
+        then:
+        1 * userService.findUserProfileByJwt(jwt)
+        1 * postService.findById(postId)
+
+        result instanceof ResponseEntity
+        result.statusCode == HttpStatus.OK
+        result.body == PostDto.toDto(currentUser, post)
+    }
+
+    @Ignore("Temporarily disabled due to userService mock issue. Need to fix")
+    def "should create a post"() {
+        given:
+        def jwt = "token"
+        def currentUser = new User(id: 1, fullName: "testUser")
+        def post = new Post()
+        def createdPost = new Post()
+
+        userService.findUserProfileByJwt(jwt) >> currentUser
+        postService.createPost(post, currentUser) >> createdPost
+
+        when:
+        def result = postController.createPost(post, jwt)
+
+        then:
+        1 * userService.findUserProfileByJwt(jwt)
+        1 * postService.createPost(post, currentUser)
+
+        result instanceof ResponseEntity
+        result.statusCode == HttpStatus.CREATED
+        result.body == PostDto.toDto(currentUser, createdPost)
+    }
+
+    @Ignore("Temporarily disabled due to userService mock issue. Need to fix")
+    def "should update a post"() {
+        given:
+        def postId = "postId"
+        def jwt = "token"
+        def currentUser = new User(id: 1, fullName: "testUser")
+        def post = new Post()
+        def editedPost = new Post()
+
+        userService.findUserProfileByJwt(jwt) >> currentUser
+        postService.editPost(postId, post, currentUser) >> editedPost
+
+        when:
+        def result = postController.updatePost(postId, post, jwt)
+
+        then:
+        1 * userService.findUserProfileByJwt(jwt)
+        1 * postService.editPost(postId, post, currentUser)
+
+        result instanceof ResponseEntity
+        result.statusCode == HttpStatus.ACCEPTED
+        result.body == PostDto.toDto(currentUser, editedPost)
+    }
+
+    @Ignore("Temporarily disabled due to userService mock issue. Need to fix")
+    def "should create a comment"() {
+        given:
+        def jwt = "token"
+        def currentUser = new User(id: 1, fullName: "testUser")
+        def commentRequest = new CommentRequest()
+        def post = new Post()
+
+        userService.findUserProfileByJwt(jwt) >> currentUser
+        postService.createComment(commentRequest, currentUser) >> post
+
+        when:
+        def result = postController.createComment(commentRequest, jwt)
+
+        then:
+        1 * userService.findUserProfileByJwt(jwt)
+        1 * postService.createComment(commentRequest, currentUser)
+
+        result instanceof ResponseEntity
+        result.statusCode == HttpStatus.CREATED
+        result.body == PostDto.toDto(currentUser, post)
+    }
+
+    @Ignore("Temporarily disabled due to userService mock issue. Need to fix")
+    def "should delete a post"() {
+        given:
+        def postId = "postId"
+        def jwt = "token"
+        def currentUser = new User(id: 1, fullName: "testUser")
+
+        userService.findUserProfileByJwt(jwt) >> currentUser
+        postService.deletePostById(postId, currentUser)
+
+        when:
+        def result = postController.deletePost(postId, jwt)
+
+        then:
+        1 * userService.findUserProfileByJwt(jwt)
+        1 * postService.deletePostById(postId, currentUser)
+
+        result instanceof ResponseEntity
+        result.statusCode == HttpStatus.NO_CONTENT
+        result.body == null
+    }
 }
--- src/test/groovy/com/example/demo/service/LikeServiceTest.groovy
+++ src/test/groovy/com/example/demo/service/LikeServiceTest.groovy
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.context.junit4.SpringRunner
+import spock.lang.Ignore
 import spock.lang.Specification
 
 @RunWith(SpringRunner.class)
@@ -33,67 +34,70 @@ class LikeServiceTest extends Specification {
         likeService
     }
 
-//    def "should add a like to a post"() {
-//        given:
-//
-//        def postId = "post123"
-//        def user = new User(id: 1, fullName: "testUser")
-//        def post = new Post(id: postId, likes: [])
-//        postService.findById(postId) >> new Post()
-//        likeRepository.save(_) >> { Like like -> like }
-//
-//        when:
-//        def result = likeService.likePost(postId, user)
-//
-//        then:
-//        1 * postService.findById(postId)
-//        1 * likeRepository.save(_)
-//        1 * postRepository.save(_)
-//
-//        result instanceof Like
-//        result.post == post
-//        result.user == user
-//        post.getLikes() == [result]
-//    }
-//
-//    def "should remove a like from a post"() {
-//        given:
-//        def postId = "post123"
-//        def user = new User(id: 1, fullName: "testUser")
-//
-//        def existingLike = new Like(id: "like123", post: new Post(id: postId), user: user)
-//        def post = new Post(id: postId, likes: [existingLike])
-//        postService.findById(postId) >> post
-//        likeRepository.deleteById(existingLike.id)
-//
-//        when:
-//        def result = likeService.likePost(postId, user)
-//
-//        then:
-//        1 * postService.findById(postId)
-//        1 * likeRepository.deleteById(existingLike.id)
-//        1 * postRepository.save(_)
-//
-//        result == null
-//        post.getLikes() == []
-//    }
-//
-//    def "should throw IllegalAccessException when attempting to delete another user's like"() {
-//        given:
-//        def postId = "post123"
-//        def currentUser = new User(id: 1, fullName: "currentUser")
-//        def anotherUser = new User(id: 2, fullName: "anotherUser")
-//
-//        def existingLike = new Like(id: "like123", post: new Post(id: postId), user: anotherUser)
-//        def post = new Post(id: postId, likes: [existingLike])
-//        postService.findById(postId) >> post
-//
-//        when:
-//        likeService.likePost(postId, currentUser)
-//
-//        then:
-//        1 * postService.findById(postId)
-//
-//        thrown(IllegalAccessException)
-//    }
+    @Ignore("Temporarily disabled due to userService mock issue. Need to fix")
+    def "should add a like to a post"() {
+        given:
+
+        def postId = "post123"
+        def user = new User(id: 1, fullName: "testUser")
+        def post = new Post(id: postId, likes: [])
+        postService.findById(postId) >> new Post()
+        likeRepository.save(_) >> { Like like -> like }
+
+        when:
+        def result = likeService.likePost(postId, user)
+
+        then:
+        1 * postService.findById(postId)
+        1 * likeRepository.save(_)
+        1 * postRepository.save(_)
+
+        result instanceof Like
+        result.post == post
+        result.user == user
+        post.getLikes() == [result]
+    }
+
+    @Ignore("Temporarily disabled due to userService mock issue. Need to fix")
+    def "should remove a like from a post"() {
+        given:
+        def postId = "post123"
+        def user = new User(id: 1, fullName: "testUser")
+
+        def existingLike = new Like(id: "like123", post: new Post(id: postId), user: user)
+        def post = new Post(id: postId, likes: [existingLike])
+        postService.findById(postId) >> post
+        likeRepository.deleteById(existingLike.id)
+
+        when:
+        def result = likeService.likePost(postId, user)
+
+        then:
+        1 * postService.findById(postId)
+        1 * likeRepository.deleteById(existingLike.id)
+        1 * postRepository.save(_)
+
+        result == null
+        post.getLikes() == []
+    }
+
+    @Ignore("Temporarily disabled due to userService mock issue. Need to fix")
+    def "should throw IllegalAccessException when attempting to delete another user's like"() {
+        given:
+        def postId = "post123"
+        def currentUser = new User(id: 1, fullName: "currentUser")
+        def anotherUser = new User(id: 2, fullName: "anotherUser")
+
+        def existingLike = new Like(id: "like123", post: new Post(id: postId), user: anotherUser)
+        def post = new Post(id: postId, likes: [existingLike])
+        postService.findById(postId) >> post
+
+        when:
+        likeService.likePost(postId, currentUser)
+
+        then:
+        1 * postService.findById(postId)
+
+        thrown(IllegalAccessException)
+    }
 }
